93b7443b903343a5b0cbb1bb31b2fa9c27e6c180
choihyejung <45138707+sunjeong18@users.noreply.github.com>
2020-04-12 23:06:59 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,28 +1,45 @@
-# 실습 내용에 추가로 코드를 짜는게 어려워서 개인적으로 공부하면서 BeautifulSoup로 웹사이트 크롤링 하는 것을 해 보았습니다.
+# 실행하기 위해 필요한 패키지 목록
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
-import urllib.request
-
-def get_soup(target_url):
-    html = urllib.request.urlopen(target_url).read()
-    soup = BeautifulSoup(html, 'html.parser')
-    return soup
-
-#<table class="grid" style="width:800px"> 태그
-
-def extract_data(soup):
-    table = soup.find('table', {'class': 'grid'})
-    trs = table.find_all('tr')
-    for idx, tr in enumerate(trs):
-        if idx > 0:
-            tds = tr.find_all('td')
-            sequence = tds[0].text.strip()
-            description = tds[1].text.strip()
-            solved_num = tds[2].text.strip()
-            print(sequence, description, solved_num)
-
-for i in range(1, 7):
-    target_url = 'http://euler.synap.co.kr/prob_list.php?pg={}'.format(i)
-    soup = get_soup(target_url)
-    extract_data(soup)
 
 
+url = "https://book.naver.com/bestsell/bestseller_list.nhn"
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.implicitly_wait(30)
+
+
+# 네이버의 베스트셀러 웹페이지를 가져오기
+driver.get(url)
+bsObject = BeautifulSoup(driver.page_source, 'html.parser')
+
+
+
+# 책의 상세 웹페이지 주소를 추출하여 리스트에 저장
+book_page_urls = []
+for index in range(0, 25):
+    dl_data = bsObject.find('dt', {'id':"book_title_"+str(index)})
+    link = dl_data.select('a')[0].get('href')
+    book_page_urls.append(link)
+
+
+
+# 메타 정보와 본문에서 필요한 정보를 추출
+for index, book_page_url in enumerate(book_page_urls):
+
+    driver.get(book_page_url)
+    bsObject = BeautifulSoup(driver.page_source, 'html.parser')
+
+
+    title = bsObject.find('meta', {'property':'og:title'}).get('content')
+    author = bsObject.find('dt', text='저자').find_next_siblings('dd')[0].text.strip()
+    image = bsObject.find('meta', {'property':'og:image'}).get('content')
+    url = bsObject.find('meta', {'property':'og:url'}).get('content')
+
+    dd = bsObject.find('dt', text='가격').find_next_siblings('dd')[0]
+    salePrice = dd.select('div.lowest strong')[0].text
+    originalPrice = dd.select('div.lowest span.price')[0].text
+
+    print(index+1, title, author, image, url, originalPrice, salePrice)
